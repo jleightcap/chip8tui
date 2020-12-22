09687b5abe2d71208afcdcd02eb45fe0265850c7
jleightcap <jleightcap@gmail.com>
2020-12-22 16:46:29 -0500

    stack operations, non-I/O opcodes
    - all opcodes implemented and tested except I/O (key, sprite, delay, etc)
    - add stack memory to Cpu struct
    - call/ret stack handling

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,33 +1,42 @@
 const OP_LEN: usize = 2; // number of words in an opcode
 enum PC {
-    I,          // pc += 1*OP_LEN
-    C,          // pc += 2*OP_LEN (condition, skip jump instrution)
-    J(usize),   // pc += arg
+    I,           // pc += 1*OP_LEN (increment)
+    C,           // pc += 2*OP_LEN (condition, skip jump instrution)
+    RJ(usize),   // pc += arg (relative jump)
+    AJ(usize),   // pc  = arg (absolute jump)
 }
 impl PC {
     fn cond(q: bool) -> PC {
-        if q { PC::C }
-        else { PC::I }
+        if q { PC::I }
+        else { PC::C }
     }
 }
 
-const RAM_SIZE:  usize = 4096;
-const REG_COUNT: usize = 16;
+const STACK_SIZE: usize = 16;
+const RAM_SIZE:   usize = 4096;
+const REG_COUNT:  usize = 16;
 pub struct Cpu {
+    /* memory */
+    ram:    [u8; RAM_SIZE],     // RAM tape
+
+    /* stack */
+    sp:     usize,              // stack pointer
+    s:      [usize; STACK_SIZE],// stack memory
+
+    /* registers */
+    v:      [u8; REG_COUNT],    // general registers
     i:      usize,              // index register
     pc:     usize,              // program counter
-    sp:     usize,              // stack pointer
-    ram:    [u8; RAM_SIZE],     // RAM tape
-    v:      [u8; REG_COUNT],    // registers
 }
 impl Cpu {
     pub fn new() -> Self {
         //let mut ram = [0; RAM_SIZE];
         Cpu {
-            i:      0,
-            pc:     0x200,
-            sp:     0,
             ram:    [0; RAM_SIZE],
+            sp:     0x0,
+            s:      [0; STACK_SIZE],
+            i:      0x000,
+            pc:     0x200,
             v:      [0; REG_COUNT],
         }
     }
@@ -61,71 +70,73 @@ impl Cpu {
         let n   = nibs.3 as usize;        // opcode argument
 
         let cycle_count: PC = match nibs {
-            (0x0, 0x0, 0xe, 0x0) => /* clear */
+            (0x0, 0x0, 0xe, 0x0) =>     /* clear */
                 self.op_00e0(),
-            (0x0, 0x0, 0xe, 0xe) => /* return */
+            (0x0, 0x0, 0xe, 0xe) =>     /* return */
                 self.op_00ee(),
-            (0x1, _,   _,   _  ) => /* jump nnn */
+            (0x1, _,   _,   _  ) =>     /* jump nnn */
                 self.op_1nnn(nnn),
-            (0x2, _,   _,   _  ) => /* call nnn */
+            (0x2, _,   _,   _  ) =>     /* call nnn */
                 self.op_2nnn(nnn),
-            (0x3, _,   _,   _  ) => /* if vx != nn then */
+            (0x3, _,   _,   _  ) =>     /* if vx != nn then */
                 self.op_3xnn(x, nn),
-            (0x4, _,   _,   _  ) => /* if vx == nn then */
+            (0x4, _,   _,   _  ) =>     /* if vx == nn then */
                 self.op_4xnn(x, nn),
-            (0x5, _,   _,   _  ) => /* if vx != vy then */
+            (0x5, _,   _,   _  ) =>     /* if vx != vy then */
                 self.op_5xy0(x, y),
-            (0x6, _,   _,   _  ) => /* vx := nn */
+            (0x6, _,   _,   _  ) =>     /* vx := nn */
                 self.op_6xnn(x, nn),
-            (0x7, _,   _,   _  ) => /*  vx += nn */
+            (0x7, _,   _,   _  ) =>     /*  vx += nn */
                 self.op_7xnn(x, nn),
-            (0x8, _,   _,   0x0) => /* vx := vy */
+            (0x8, _,   _,   0x0) =>     /* vx := vy */
                 self.op_8xy0(x, y),
-            (0x8, _,   _,   0x1) => /* vx |= vy */
+            (0x8, _,   _,   0x1) =>     /* vx |= vy */
                 self.op_8xy1(x, y),
-            (0x8, _,   _,   0x2) => /* vx &= vy */
+            (0x8, _,   _,   0x2) =>     /* vx &= vy */
                 self.op_8xy2(x, y),
-            (0x8, _,   _,   0x3) => /* vx ^= vy */
+            (0x8, _,   _,   0x3) =>     /* vx ^= vy */
                 self.op_8xy3(x, y),
-            (0x8, _,   _,   0x4) => /* vx += vy */
+            (0x8, _,   _,   0x4) =>     /* vx += vy */
                 self.op_8xy4(x, y),
-            (0x8, _,   _,   0x5) => /* vx -= vy */
+            (0x8, _,   _,   0x5) =>     /* vx -= vy */
                 self.op_8xy5(x, y),
-            (0x8, _,   _,   0x6) => /* vx >>= vy */
+            (0x8, _,   _,   0x6) =>     /* vx >>= vy */
                 self.op_8xy6(x, y),
-            (0x8, _,   _,   0x7) => /* vx = -vy */
+            (0x8, _,   _,   0x7) =>     /* vx = -vy */
                 self.op_8xy7(x, y),
-            (0x8, _,   _,   0xe) => /* vx <<= vy */
+            (0x8, _,   _,   0xe) =>     /* vx <<= vy */
                 self.op_8xye(x, y),
-            (0x9, _,   _,   0x0) => /* if vx == vy then */
+            (0x9, _,   _,   0x0) =>     /* if vx == vy then */
                 self.op_9xy0(x, y),
-            (0xa, _,   _,   _  ) => /* i := nnn */
+            (0xa, _,   _,   _  ) =>     /* i := nnn */
                 self.op_annn(nnn),
-            (0xb, _,   _,   _  ) => /* jump nnn + v0 */
+            (0xb, _,   _,   _  ) =>     /* jump nnn + v0 */
                 self.op_bnnn(nnn),
-            (0xc, _,   _,   _  ) => /* vx := random(0, 255) & nn */
+            (0xc, _,   _,   _  ) =>     /* vx := random(0, 255) & nn */
                 self.op_cxnn(x, nn),
-            (0xd, _,   _,   _  ) => /* sprite vx vy n */
+            (0xd, _,   _,   _  ) =>     /* sprite vx vy n */
                 self.op_dxyn(x, y, n),
-            (0xe, _,   0x9, 0xe) => /* is a key not pressed? */
+            (0xe, _,   0x9, 0xe) =>     /* is a key not pressed? */
                 self.op_ex9e(x),
-            (0xe, _,   0xa, 0xe) => /* is a key pressed? */
+            (0xe, _,   0xa, 0xe) =>     /* is a key pressed? */
                 self.op_ex9e(x),
-            (0xf, _,   0x0, 0x7) => /* vx := delay */
+            (0xf, _,   0x0, 0x7) =>     /* vx := delay */
                 self.op_fx07(x),
-            (0xf, _,   0x0, 0xa) => /* vx := key */
+            (0xf, _,   0x0, 0xa) =>     /* vx := key */
                 self.op_fx0a(x),
-            (0xf, _,   0x1, 0x5) => /* delay := vx */
+            (0xf, _,   0x1, 0x5) =>     /* delay := vx */
                 self.op_fx15(x),
-            (0xf, _,   0x1, 0x8) => /* buzzer := vx */
+            (0xf, _,   0x1, 0x8) =>     /* buzzer := vx */
                 self.op_fx18(x),
-            (0xf, _,   0x2, 0x9) => /* i := hex(vx) */
+            (0xf, _,   0x1, 0xe) =>     /* buzzer := vx */
+                self.op_fx1e(x),
+            (0xf, _,   0x2, 0x9) =>     /* i := hex(vx) */
                 self.op_fx29(x),
-            (0xf, _,   0x3, 0x3) => /* bcd vx */
+            (0xf, _,   0x3, 0x3) =>     /* bcd vx */
                 self.op_fx33(x),
-            (0xf, _,   0x5, 0x5) => /* save vx */
+            (0xf, _,   0x5, 0x5) =>     /* save vx */
                 self.op_fx55(x),
-            (0xf, _,   0x6, 0x5) => /* load vx */
+            (0xf, _,   0x6, 0x5) =>     /* load vx */
                 self.op_fx55(x),
             (_,   _,   _,   _  ) => {
                 panic!("unexpected opcode!");
@@ -133,44 +144,54 @@ impl Cpu {
         };
 
         match cycle_count {
-            PC::I    => self.pc = self.pc.wrapping_add(1*OP_LEN),
-            PC::C    => self.pc = self.pc.wrapping_add(2*OP_LEN),
-            PC::J(a) => self.pc =  a,
+            PC::I     => self.pc  = self.pc.wrapping_add(1*OP_LEN),
+            PC::C     => self.pc  = self.pc.wrapping_add(2*OP_LEN),
+            PC::RJ(a) => self.pc += self.pc.wrapping_add(a),
+            PC::AJ(a) => self.pc  = a,
         }
     }
 
     fn op_00e0(&mut self) -> PC {
-        // TODO
+        // TODO: clear
         PC::I
     }
 
+    // return
     fn op_00ee(&mut self) -> PC {
-        PC::I
+        self.pc -= 1;
+        PC::AJ(self.s[self.sp])
     }
 
+    // pc = nnn
     fn op_1nnn(&mut self, nnn: usize) -> PC {
-        PC::I
+        PC::AJ(nnn)
     }
 
     fn op_2nnn(&mut self, nnn: usize) -> PC {
-        PC::I
+        self.s[self.sp] = self.pc + OP_LEN;
+        self.sp += 1;
+        PC::AJ(nnn)
     }
 
+    // if v[x] != nn then
     fn op_3xnn(&mut self, x: usize, nn: u8) -> PC {
-        PC::I
+        PC::cond(self.v[x] != nn)
     }
 
+    // if v[x] == nn then
     fn op_4xnn(&mut self, x: usize, nn: u8) -> PC {
-        PC::I
+        PC::cond(self.v[x] == nn)
     }
 
+    // if v[x] != v[y] then
     fn op_5xy0(&mut self, x: usize, y: usize) -> PC {
-        PC::I
+        PC::cond(self.v[x] != self.v[y])
     }
 
     // v[x] = nn
     fn op_6xnn(&mut self, x: usize, nn: u8) -> PC {
-        self.v[x] = nn; PC::I
+        self.v[x] = nn;
+        PC::I
     }
 
     // v[x] += nn
@@ -182,22 +203,26 @@ impl Cpu {
 
     // v[x] = v[y]
     fn op_8xy0(&mut self, x: usize, y: usize) -> PC {
-        self.v[x] = self.v[y]; PC::I
+        self.v[x] = self.v[y];
+        PC::I
     }
 
     // v[x] |= v[y]
     fn op_8xy1(&mut self, x: usize, y: usize) -> PC {
-        self.v[x] |= self.v[y]; PC::I
+        self.v[x] |= self.v[y];
+        PC::I
     }
 
     // v[x] &= v[y]
     fn op_8xy2(&mut self, x: usize, y: usize) -> PC {
-        self.v[x] &= self.v[y]; PC::I
+        self.v[x] &= self.v[y];
+        PC::I
     }
 
     // v[x] ^= v[y]
     fn op_8xy3(&mut self, x: usize, y: usize) -> PC {
-        self.v[x] ^= self.v[y]; PC::I
+        self.v[x] ^= self.v[y];
+        PC::I
     }
 
     // v[x] += v[y]
@@ -217,7 +242,7 @@ impl Cpu {
     // v[x] >>= v[y]
     fn op_8xy6(&mut self, x: usize, y: usize) -> PC {
         let res = (self.v[x] as u16) >> (self.v[y] as u16);
-        self.v[x] = res as u8;
+        self.v[x] = res as u8; // does this underflow?
         return PC::I
     }
 
@@ -230,37 +255,52 @@ impl Cpu {
 
     // v[x] <<= v[y]
     fn op_8xye(&mut self, x: usize, y: usize) -> PC {
+        let res = (self.v[x] as u16) << (self.v[y] as u16);
+        self.v[x] = res as u8; // does this overflow?
         PC::I
     }
 
+    // if v[x] == v[y] then
     fn op_9xy0(&mut self, x: usize, y: usize) -> PC {
-        PC::I
+        PC::cond(self.v[x] == self.v[y])
     }
 
+    // i = nnn
     fn op_annn(&mut self, nnn: usize) -> PC {
+        self.i = nnn;
         PC::I
     }
 
+    // pc = nnn + v[0]
     fn op_bnnn(&mut self, nnn: usize) -> PC {
-        PC::I
+        PC::AJ(nnn + self.v[0] as usize)
     }
 
+    // v[x] = rand(255) & nn
     fn op_cxnn(&mut self, x: usize, nn: u8) -> PC {
+        // TODO: random
         PC::I
     }
 
+    // sprite v[x] v[y] n
     fn op_dxyn(&mut self, x: usize, y: usize, n: usize) -> PC {
+        // TODO: sprite
         PC::I
     }
 
+    // if v[x] != key then
     fn op_ex9e(&mut self, x: usize) -> PC {
+        // TODO: key
         PC::I
     }
 
+    // if v[s] == key then
     fn op_exa1(&mut self, x: usize) -> PC {
+        // TODO: key
         PC::I
     }
 
+    // v[x] = delay
     fn op_fx07(&mut self, x: usize) -> PC {
         PC::I
     }
@@ -277,7 +317,10 @@ impl Cpu {
         PC::I
     }
 
+    // i += v[x]
     fn op_fx1e(&mut self, x: usize) -> PC {
+        let res = self.v[x].wrapping_add(self.i as u8);
+        self.i = res as usize;
         PC::I
     }
 
--- src/cpu_test.rs
+++ src/cpu_test.rs
@@ -12,6 +12,7 @@ fn exec_test_prog(p: &Vec<u16>, c: &mut Cpu) {
 fn test_init() {
     let c: Cpu = Cpu::new();
     assert_eq!(c.pc, 0x200);
+    assert_eq!(c.sp, 0x0);
 }
 
 
@@ -20,6 +21,62 @@ fn test_init() {
 // OPCODE TEST
 //
 // ========================================================================= //
+
+#[test]
+fn test_0x00ee() {
+    let mut c = Cpu::new();
+    c.s[0] = 0x200;
+    exec_test_prog(&vec![0x00ee], &mut c);
+    assert_eq!(c.sp, 0x0); // return address poped from stack
+    assert_eq!(c.pc, 0x200); // pc at value poped
+}
+
+#[test]
+fn test_0x2nnn() {
+    let mut c = Cpu::new();
+    exec_test_prog(&vec![0x2242], &mut c);
+    assert_eq!(c.sp, 0x1); // return address on stack
+    assert_eq!(c.s[c.sp - 1], 0x202); // return address after call
+    assert_eq!(c.pc, 0x242); // pc at called address
+}
+
+#[test]
+fn test_0x1nnn() {
+    let mut c = Cpu::new();
+    exec_test_prog(&vec![0x1242], &mut c);
+    assert_eq!(c.pc, 0x242); // pc = nnn
+}
+
+#[test]
+fn test_0x3xnn() {
+    let mut c = Cpu::new();
+    exec_test_prog(&vec![0x6022, 0x3021], &mut c);
+    assert_eq!(c.pc, 0x204); // v[x] != nn, pc + 1*OP_LEN
+    let mut c = Cpu::new();
+    exec_test_prog(&vec![0x6021, 0x3021], &mut c);
+    assert_eq!(c.pc, 0x206); // v[x] == nn, pc + 2*OP_LEN
+}
+
+#[test]
+fn test_0x4xnn() {
+    let mut c = Cpu::new();
+    exec_test_prog(&vec![0x6022, 0x4022], &mut c);
+    assert_eq!(c.pc, 0x204); // v[x] == nn, pc + 1*OP_LEN
+    let mut c = Cpu::new();
+    exec_test_prog(&vec![0x6022, 0x4021], &mut c);
+    assert_eq!(c.pc, 0x206); // v[x] != nn, pc + 2*OP_LEN
+}
+
+#[test]
+fn test_0x5xy0() {
+    let mut c = Cpu::new();
+    exec_test_prog(&vec![0x6022, 0x6123, 0x5010], &mut c);
+    assert_eq!(c.pc, 0x206); // v[x] != v[y], pc + 1*OP_LEN
+    let mut c = Cpu::new();
+    exec_test_prog(&vec![0x6022, 0x6122, 0x5010], &mut c);
+    assert_eq!(c.pc, 0x208); // v[x] == v[y], pc + 2*OP_LEN
+}
+
 #[test]
 fn test_0x6xnn() {
     // 0x6xnn => vx := nn
@@ -110,4 +167,46 @@ fn test_0x8xy7() {
     assert_eq!(c.pc, 0x206);
     assert_eq!(c.v[1], 0x62);
     assert_eq!(c.v[0], 0x42); // 0x62 - 0x20
+}
+
+#[test]
+fn test_0x8xye() {
+    let mut c = Cpu::new();
+    exec_test_prog(&vec![0x6021, 0x6101, 0x801e], &mut c);
+    assert_eq!(c.pc, 0x206);
+    assert_eq!(c.v[1], 0x01);
+    assert_eq!(c.v[0], 0x42); // 0x21 << 0x01
+}
+
+#[test]
+fn test_0x9xy0() {
+    let mut c = Cpu::new();
+    exec_test_prog(&vec![0x6021, 0x6121, 0x9010], &mut c);
+    assert_eq!(c.pc, 0x206); // v[x] == v[y], pc + 1*OP_LEN
+    let mut c = Cpu::new();
+    exec_test_prog(&vec![0x6021, 0x6122, 0x9010], &mut c);
+    assert_eq!(c.pc, 0x208); // v[x] != v[y], pc + 2*OP_LEN
+}
+
+#[test]
+fn test_0xannn() {
+    let mut c = Cpu::new();
+    exec_test_prog(&vec![0xa042], &mut c);
+    assert_eq!(c.pc, 0x202);
+    assert_eq!(c.i, 0x042); // i = nnn
+}
+
+#[test]
+fn test_0xbnnn() {
+    let mut c = Cpu::new();
+    exec_test_prog(&vec![0x6021, 0xb221], &mut c);
+    assert_eq!(c.pc, 0x242); // pc = v[0] + nnn
+}
+
+#[test]
+fn test_0xfx1e() {
+    let mut c = Cpu::new();
+    exec_test_prog(&vec![0x6042, 0xf01e], &mut c);
+    assert_eq!(c.pc, 0x204);
+    assert_eq!(c.i, 0x0042); // i += v[x]
 }
